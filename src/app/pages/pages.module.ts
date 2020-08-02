7beaf7340da89e4bfdd58df36e8e636a6fa8e7b5
Michael Ocampo <imboocampo19@gmail.com>
2020-08-02 11:17:20 +0800

    Remove unnecessary files

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,19 +3,17 @@ import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
-import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { FormsModule } from '@angular/forms';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
+    FormsModule,
     ECommerceModule,
-    MiscellaneousModule,
   ],
   declarations: [
     PagesComponent,
